04fcc27208e10ad4cd943442ed2eec3fc1234875
Aggelos Nikolaidis <=>
2024-11-17 12:21:20 +0200

    Aggelos Panagiotis Nikolaidis: t8220105

--- AM.java
+++ AM.java
@@ -5,5 +5,6 @@ public class AM {
         System.out.println("Efthymia Iordanidi: t8230042");
         System.out.println("Anastasia Karra: t8230052");
         System.out.println("Maria Kikrili: t8230058");
+        System.out.println("Aggelos Panagiotis Nikolaidis: t8220105");
     }
 }
