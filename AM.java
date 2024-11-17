baa357d03ade4cf7341c2e512bc653cd2f875fdf
Efthymia Iordanidi <t8230042@aueb.gr>
2024-11-17 11:20:14 +0200

    AM

--- AM.java
+++ AM.java
@@ -2,5 +2,6 @@ public class AM {
     public static void main(String[] args) {
         System.out.println("Melina Fatourou: t8230157");
         System.out.println("Bachas Gerasimos-Marios: t8230102");
+        System.out.println("Efthymia Iordanidi: t8230042");
     }
 }
