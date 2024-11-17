cbe19772c12e946c281e529022d4a41d014a4b04
Makis Bahas <t8230102@aueb.gr>
2024-11-17 11:14:27 +0200

    AM

--- AM.java
+++ AM.java
@@ -1,5 +1,6 @@
 public class AM {
     public static void main(String[] args) {
-        System.out.println("Melina Fatourou: t8230157")
+        System.out.println("Melina Fatourou: t8230157");
+        System.out.println("Bachas Gerasimos-Marios: t8230102");
     }
 }
