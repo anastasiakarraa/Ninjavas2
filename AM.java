582e382aee006e33089c918675ae7753fd3347aa
PetrakisGiannis <t8220123@aueb.gr>
2024-11-18 15:29:38 +0200

    Finishing the assignment.

--- AM.java
+++ AM.java
@@ -1,11 +1,12 @@
 public class AM {
     public static void main(String[] args) {
-        System.out.println("Melina Fatourou: t8230157");
-        System.out.println("Bachas Gerasimos-Marios: t8230102");
-        System.out.println("Efthymia Iordanidi: t8230042");
-        System.out.println("Anastasia Karra: t8230052");
-        System.out.println("Maria Kikrili: t8230058");
-        System.out.println("Aggelos Panagiotis Nikolaidis: t8220105");
-        System.out.println("Alexandros Tsompanis: t8220156");
+        System.out.println("Melina Fatourou: 8230157");
+        System.out.println("Bachas Gerasimos-Marios: 8230102");
+        System.out.println("Petrakis Ioannis: 8220123");
+        System.out.println("Efthymia Iordanidi: 8230042");
+        System.out.println("Anastasia Karra: 8230052");
+        System.out.println("Maria Kikrili: 8230058");
+        System.out.println("Aggelos Panagiotis Nikolaidis: 8220105");
+        System.out.println("Alexandros Tsompanis: 8220156");
     }
 }
