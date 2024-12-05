e04a17521b5c60c74e9fedf580a430cd69f13637
PetrakisGiannis <t8220123@aueb.gr>
2024-12-05 16:13:31 +0200

    learning

--- AM.java
+++ AM.java
@@ -2,7 +2,7 @@ public class AM {
     public static void main(String[] args) {
         System.out.println("Melina Fatourou: 8230157");
         System.out.println("Bachas Gerasimos-Marios: 8230102");
-        System.out.println("Petrakis Ioannis: 8220123");
+        System.out.println("Petrakis Giannis: 8220123");
         System.out.println("Efthymia Iordanidi: 8230042");
         System.out.println("Anastasia Karra: 8230052");
         System.out.println("Maria Kikrili: 8230058");
